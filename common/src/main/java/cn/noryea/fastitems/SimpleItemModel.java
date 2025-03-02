131858cb8997ef3b91dfb20aaf945225bddb8318
noryea <864460295@qq.com>
2025-03-02 12:25:10 +0800

    1.20.1 Forge

--- common/src/main/java/cn/noryea/fastitems/SimpleItemModel.java
+++ common/src/main/java/cn/noryea/fastitems/SimpleItemModel.java
@@ -3,6 +3,7 @@ package cn.noryea.fastitems;
 import com.google.common.collect.ImmutableList;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.ItemOverrides;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BakedModel;
@@ -59,6 +60,11 @@ public class SimpleItemModel implements BakedModel {
     }
 
     @Override
+    public boolean isCustomRenderer() {
+        return flattenedItem.isCustomRenderer();
+    }
+
+    @Override
     public @NotNull TextureAtlasSprite getParticleIcon() {
         return flattenedItem.getParticleIcon();
     }
@@ -66,6 +72,11 @@ public class SimpleItemModel implements BakedModel {
     @Override
     public @NotNull ItemTransforms getTransforms() {
         return flattenedItem.getTransforms();
+    }
+
+    @Override
+    public @NotNull ItemOverrides getOverrides() {
+        return flattenedItem.getOverrides();
     }
 
 }
--- common/src/main/java/cn/noryea/fastitems/mixin/ItemRendererMixin.java
+++ common/src/main/java/cn/noryea/fastitems/mixin/ItemRendererMixin.java
@@ -7,16 +7,15 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.ItemRenderer;
-import net.minecraft.client.renderer.item.ItemStackRenderState;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
@@ -24,19 +23,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ItemRendererMixin {
 
     @Unique
-    private static final SimpleItemModel fastitems$flattenedModel = new SimpleItemModel();
+    private final SimpleItemModel fastitems$flattenedModel = new SimpleItemModel();
 
     @Unique
-    private static ItemDisplayContext fastitems$displayContext;
+    private ItemDisplayContext fastitems$displayMode;
 
-    @Inject(method = "renderItem", at = @At("HEAD"))
-    private static void getRenderType(ItemDisplayContext itemDisplayContext, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j, int[] is, BakedModel bakedModel, RenderType renderType, ItemStackRenderState.FoilType foilType, CallbackInfo ci) {
-        fastitems$displayContext = itemDisplayContext;
+    @Inject(method = "render*", at = @At("HEAD"))
+    private void getRenderType(ItemStack itemStack, ItemDisplayContext itemDisplayContext, boolean bl, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j, BakedModel bakedModel, CallbackInfo ci) {
+        this.fastitems$displayMode = itemDisplayContext;
     }
 
-    @ModifyArg(method = "renderItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;renderModelLists(Lnet/minecraft/client/resources/model/BakedModel;[IIILcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;)V"), index = 0)
-    private static BakedModel ZuseFlattenItem(BakedModel model) {
-        if(FastItemsConfig.enable && !FastItemsConfig.renderSidesOfItems && !model.isGui3d() && fastitems$displayContext.equals(ItemDisplayContext.GROUND)) {
+    @ModifyVariable(method = "renderModelLists", at = @At("HEAD"), index = 1, argsOnly = true)
+    private BakedModel useFlattenItem(BakedModel model, BakedModel bakedModel, ItemStack itemStack, int i, int j, PoseStack poseStack, VertexConsumer vertexConsumer) {
+        if(FastItemsConfig.enable && !FastItemsConfig.renderSidesOfItems && !itemStack.isEmpty() && !model.isGui3d() && fastitems$displayMode == ItemDisplayContext.GROUND) {
             fastitems$flattenedModel.setItem(model);
             return fastitems$flattenedModel;
         } else
